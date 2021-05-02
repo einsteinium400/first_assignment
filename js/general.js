3c7d297c6899f873751c4b9c1539f671889322c9
einsteinium400 <sdana1933@gmail.com>
2021-05-02 22:56:14 +0300

    js, and addclasses normalClass

--- js/general.js
+++ js/general.js
@@ -1,17 +1,4 @@
 
-
-// function myFunction(x) {
-//     if (x.matches) { // If media query matches
-//       document.body.style.backgroundColor = "yellow";
-//     } else {
-//       document.body.style.backgroundColor = "pink";
-//     }
-//   }
-  
-//   var x = window.matchMedia("(max-width: 700px)")
-//   myFunction(x) // Call listener function at run time
-//   x.addListener(myFunction) // Attach listener function on state changes
-
 function display() {
     if (document.getElementsByTagName("nav")[0].style.display == "") {
         document.getElementsByTagName("nav")[0].style.display = "flex";
@@ -26,5 +13,14 @@ function display() {
 window.onload = function () {
     var hamb = document.getElementsByClassName("Hamburger");
     hamb[0].onclick = display;
+
+    const mql = window.matchMedia("(min-width: 621px)");
+
+    mql.addEventListener("change", (e) => {
+        if (e.matches)
+            document.getElementById("hamburgersearch").style.display = "";
+
+    });
+
 };
 
