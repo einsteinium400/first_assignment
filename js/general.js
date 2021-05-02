98326d501f3c2102a7a07caf0c11230fbe80efea
einsteinium400 <sdana1933@gmail.com>
2021-05-02 15:35:08 +0300

    html validation

--- js/general.js
+++ js/general.js
@@ -3,19 +3,14 @@ function display() {
     if (document.getElementsByTagName("nav")[0].style.display == "") {
         document.getElementsByTagName("nav")[0].style.display = "flex";
         document.getElementById("hamburgersearch").style.display = "flex";
-
-
     }
     else {
         document.getElementsByTagName("nav")[0].style.display = "";
         document.getElementById("hamburgersearch").style.display = "";
     }
-
-
 }
 
 window.onload = function () {
     var hamb = document.getElementsByClassName("Hamburger");
-    // console.log("hey")
     hamb[0].onclick = display;
 };
