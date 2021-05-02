a874d9851adc31a4b4654f69bee9dd2f26b1a5d1
einsteinium400 <sdana1933@gmail.com>
2021-05-02 22:12:31 +0300

    wip

--- js/general.js
+++ js/general.js
@@ -1,4 +1,17 @@
 
+
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//       document.body.style.backgroundColor = "yellow";
+//     } else {
+//       document.body.style.backgroundColor = "pink";
+//     }
+//   }
+  
+//   var x = window.matchMedia("(max-width: 700px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addListener(myFunction) // Attach listener function on state changes
+
 function display() {
     if (document.getElementsByTagName("nav")[0].style.display == "") {
         document.getElementsByTagName("nav")[0].style.display = "flex";
@@ -14,3 +27,4 @@ window.onload = function () {
     var hamb = document.getElementsByClassName("Hamburger");
     hamb[0].onclick = display;
 };
+
