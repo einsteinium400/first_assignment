4ec0789e5320af1fd222df33642d54d100c25b2c
MishaStruk <mishasoni@gmail.com>
2021-07-01 17:49:24 +0300

    Change all to php and add login page not completed

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <script src="js/dashboardscript.js"></script>
+    <script src="js/general.js"></script>
+
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <header class="wrapper">
+        <a href="#" id="logo"></a>
+        <input type="text" name="searchItems" id="searchBar" placeholder="Search students, classes, reports...">
+        <button type="submit" value="Search" class="search-btn">Search</button>
+        <div id="userData">
+            <p id="nameGreeting">Hello, Ron Azulai</p>
+            <p id="time">07:52</p>
+        </div>
+
+        <a href="#" class="Hamburger">
+            <div></div>
+            <div></div>
+            <div></div>
+        </a>
+
+
+    </header>
+
+    <div id="hamburgersearch">
+        <input type="text" name="searchItems" placeholder="Search students, classes, reports...">
+        <button type="submit" value="Search" class="search-btn">Search</button>
+    </div>
+
+
+    <nav>
+        <a href="#" class="current">
+            <p>Dashboard</p>
+        </a>
+        <a href="classeslist.html">
+            <p>Classes</p>
+        </a>
+        <a href="#">
+            <p>Pupils</p>
+        </a>
+        <a href="#">
+            <p>Reports</p>
+        </a>
+        <a href="#">
+            <p>Settings</p>
+        </a>
+    </nav>
+
+    <ul id="breadcrumb">
+        <li><a href="#">Dashboard</a></li>
+    </ul>
+
+    <h1>Dashboard</h1>
+
+    <main class="wrapper">
+        <section class="mainArea">
+            <section class="mainSection">
+                <!--Place here the main content of the page-->
+                
+                
+
+            </section>
+        </section>
+    </main>
+</body>
+
+</html>
